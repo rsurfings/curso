54f82a6c076e4df96f0960e3ce339e4428738f6f
rsurfings <rsurfings@gmail.com>
2015-11-24 01:33:19 -0200

    ajax

--- javascript/js/21.js
+++ javascript/js/21.js
@@ -1,34 +1,129 @@
 window.onload = function (){
 
+	var carregarAjax = document.getElementById('link').onclick = function carregarAjax(){
+
+		var resposta = document.getElementById("resposta");
+
+		var ajax = new XMLHttpRequest();
+
+		//console.log(ajax);
+		ajax.onreadystatechange = function(){ // ouve resposta a requisição
+	
+			//console.log(ajax.readyState); //codigo de retorno
+			switch(ajax.readyState)
+			{
+				case 0:
+					//console.log("pedido não inicializado");
+					break;
+				case 1:
+					//console.log("conexão do servidor estabelecido");
+					break;
+				case 2:
+					//console.log("pedido recebido");
+					break;
+				case 3:
+					//console.log("solicitação de processamento");
+					break;
+				case 4:
+					//console.log("solicitar resposta acabado e está pronto");
+					break;	
+				default:
+			}
+
+			//console.log(ajax.status);
+			switch(ajax.status)
+			{
+				case 200:
+					//console.log("OK");
+					break;
+				case 404:
+					//console.log("Page not found");
+					break;
+				default:
+			}
+
+			if (ajax.readyState == 4 && ajax.status == 200) {
+
+				resposta.appendChild(document.createTextNode(ajax.responseText));
+
+				//resposta.innerHTML = ajax.responseText; //responseXML
+
+				//contruirTabela(ajax);
+			}
+
+		}
+
+		ajax.open("POST",'texto.txt',true); //texto.txt | conteudo.html | catalogo_cd.xml
+		ajax.send(); //param=1&param2
+
+		return false;
+	};
+
+
+	/*
+	*	contruir Tabela
+	*/
+	function contruirTabela(ajax) {
+
+		var xml = ajax.responseXML;
+
+		var resposta = document.getElementById("resposta");
+
+		var table = '<table border="2">';
+		table += "<tr><th>Artista</th><th>Titulo</th></tr>";
+		var x = xml.getElementsByTagName("CD");
+		for (var i = 0; i <x.length; i++) { 
+		   table += "<tr><td>" +
+			x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+		    "</td><td>" +
+		    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+		    "</td></tr>";
+		 }
+
+		
+		table += '</table>';
+
+		//console.log(table);
+
+		resposta.innerHTML = table;
+	}
+
+
 	var nodes = document.querySelectorAll('.link');
 	var arr = [];
 	for (var i = 0, max = nodes.length; i < max; i++){
 		nodes[i].onclick = function (){
 			
-			requisitar(this.href);
+			fazRequisicao(this.href);
 
 			return false;
 
 		}
 	}
 
-	function mostar(ajax){
+	/*
+	*	mostra Resposta
+	*/
+	function mostraResposta(ajax){
 		var resposta = document.getElementById("resposta");
 		resposta.innerHTML = ajax.responseText;
 	}
 
-	function requisitar(url){
+	/*
+	*	faz a Requisição
+	*/
+	function fazRequisicao(url){
 
 		var ajax = iniciaAjax();
 		
 		var resposta = document.getElementById("resposta");
 
-		carregando(resposta);
+		carregarResposta(resposta);
 
 		ajax.onreadystatechange = function(){ // ouve resposta a requisição
 				if (ajax.readyState == 4 && ajax.status == 200) {
 
-					mostar(ajax);
+					mostraResposta(ajax);
 				}
 		}
 
@@ -36,6 +131,9 @@ window.onload = function (){
 		ajax.send();
 	}
 
+	/*
+	* Estancia (inicia) O Objeto
+	*/
 	function iniciaAjax(){
 		var ajax = null;
 		if(window.XMLHttpRequest){
@@ -52,86 +150,15 @@ window.onload = function (){
 		return ajax;
 	}
 
-	function carregando(resposta){
+	/*
+	* Carregar a resposta
+	*/
+	function carregarResposta(resposta){
 
 		var img = document.createElement("img");
 		img.setAttribute("src","image/ajax-loader.gif");
 
 		resposta.appendChild(img);
-	}
-
-	var catalogo = document.getElementById('catalogo').onclick = function carregarAjax(){
-
-		var ajax = new XMLHttpRequest();
-			ajax.onreadystatechange = function(){ // ouve resposta a requisição
-
-				console.log(ajax.readyState);
-				switch(ajax.readyState)
-				{
-					case 0:
-						console.log("pedido não inicializado");
-						break;
-					case 1:
-						console.log("conexão do servidor estabelecido");
-						break;
-					case 2:
-						console.log("pedido recebido");
-						break;
-					case 3:
-						console.log("solicitação de processamento");
-						break;
-					case 4:
-						console.log("solicitar resposta acabado e está pronto");
-						break;	
-					default:
-				}
-
-				console.log(ajax.status);
-				switch(ajax.status)
-				{
-					case 200:
-						console.log("OK");
-						break;
-					case 404:
-						console.log("Page not found");
-						break;
-					default:
-				}
-
-				if (ajax.readyState == 4 && ajax.status == 200) {
-					//document.getElementById("resposta").innerHTML = ajax.responseXML;
-
-					contruirTabela(ajax);
-				}
-
-			}
-
-			ajax.open("GET",'cd_catalog.xml',true);
-			ajax.send(); //param=1&param2
-
-			return false;
-	};
-
-	function contruirTabela(ajax) {
-
-	  var i;
-	  var xmlDoc = ajax.responseXML;
-	  
-	  var table = '<table border="2">';
-	  table += "<tr><th>Artist</th><th>Title</th></tr>";
-	  var x = xmlDoc.getElementsByTagName("CD");
-	  for (i = 0; i <x.length; i++) { 
-	    table += "<tr><td>" +
-	    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-	    "</td><td>" +
-	    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-	    "</td></tr>";
-	  }
-	  table += '</table>';
-
-	  console.log(table);
-
-	  document.getElementById("resposta").innerHTML = table;
 	}
 }
 
